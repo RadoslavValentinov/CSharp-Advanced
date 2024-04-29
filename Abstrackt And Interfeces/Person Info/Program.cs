7dcba1b5cdf5c1912261d3de5c8be9ed0b779f62
Radoslav Hristov <160596999+RadoslavValentinov@users.noreply.github.com>
2024-04-29 22:09:43 +0300

    Add files via upload

---
+++ Abstrackt And Interfeces/Person Info/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace PersonInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string id = Console.ReadLine();
+            string birthdate = Console.ReadLine();
+            IIdentifiable idnum = new Citizen(name,age,id,birthdate);
+            IBirthable date = new Citizen(name, age, id, birthdate);
+           
+            Console.WriteLine(idnum.Id);
+            Console.WriteLine(date.Birthdate);
+
+
+        }
+    }
+}
