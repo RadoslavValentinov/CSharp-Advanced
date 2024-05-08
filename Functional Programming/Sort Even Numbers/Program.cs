addd3a118066f26993a82c37429198d48734a8a8
Radoslav Hristov <160596999+RadoslavValentinov@users.noreply.github.com>
2024-05-08 20:48:27 +0300

    Add files via upload

---
+++ Functional Programming/Sort Even Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = Console.ReadLine().Split(", ")
+                .Select(int.Parse)
+                .Where(x=>x % 2==0)
+                .OrderBy(x=>x)
+                .ToArray();
+            Console.WriteLine(string.Join(", ",number));
+        }
+    }
+}
