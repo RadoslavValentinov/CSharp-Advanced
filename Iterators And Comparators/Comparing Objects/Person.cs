cb4ffbdce789c93c4a54849a066067ac5e967d5e
Radoslav Hristov <160596999+RadoslavValentinov@users.noreply.github.com>
2024-05-10 19:43:11 +0300

    Add files via upload

---
+++ Iterators And Comparators/Comparing Objects/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComparingObjects
+{
+    public class Person :IComparable<Person>
+    {
+        private string name;
+        private int age;
+        private string town;
+
+        public Person(string names,int ages,string towns)
+        {
+            name = names;
+            age = ages;
+            town = towns;
+        }
+
+        public string Name => name;
+
+        public int Age => age;
+
+        public string Town => town;
+
+
+        public int CompareTo(Person other)
+        {
+            int result =Name.CompareTo(other.Name);
+            if (result==0)
+            {
+                result = Age.CompareTo(other.Age);
+            }
+
+            if (result==0)
+            {
+                result = Town.CompareTo(other.Town);
+            }
+            return result;
+        }
+    }
+}
---
+++ Iterators And Comparators/Comparing Objects/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparingObjects
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+          
+            List<Person> persons = new List<Person>();
+
+            while (input !="END")
+            {
+                string[] splited = input.Split();
+                string name = splited[0];
+                int age = int.Parse(splited[1]);
+                string town = splited[2];
+
+                persons.Add(new Person(name, age, town));
+
+                input = Console.ReadLine();
+            }
+
+            int index = int.Parse(Console.ReadLine())-1;
+
+            var equal = 0;
+            var noequel = 0;
+
+            foreach (Person item in persons)
+            {
+                if (persons[index].CompareTo(item)==0)
+                {
+                    equal++;
+                }
+                else
+                {
+                    noequel++;
+                }
+            }
+
+            if (equal <= 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{equal} {noequel} {persons.Count}");
+            }
+        }
+    }
+}
