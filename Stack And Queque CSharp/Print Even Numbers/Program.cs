41c9b23d93ffd1124eebe5c2a3af2c4f3ebee889
Radoslav Hristov <160596999+RadoslavValentinov@users.noreply.github.com>
2024-05-20 20:05:20 +0300

    Add files via upload

---
+++ Stack And Queque CSharp/Print Even Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] sdplited = new int[input.Length];
+            Queue<string> evenNumber = new Queue<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] % 2==0)
+                {
+                    evenNumber.Enqueue(input[i].ToString());
+                }
+            }
+
+            Console.WriteLine(string.Join(", ",evenNumber));
+        }
+    }
+}
