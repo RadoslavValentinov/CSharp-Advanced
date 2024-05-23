ff195a3296a54e2120d8bef5752a637e33ddfea3
Radoslav Hristov <160596999+RadoslavValentinov@users.noreply.github.com>
2024-05-23 21:53:55 +0300

    Add files via upload

---
+++ Exam and Exam tranning/Survivor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02.Survivor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             // input row of jadged array
+            int row=int.Parse(Console.ReadLine());
+
+            char[][] beach = new char[row][];
+            
+            for (int i = 0; i < row; i++)
+            {
+                var line = Console.ReadLine().ToCharArray();
+                beach[row] = line;
+            }
+
+        }
+    }
+}
