5839fa38960aaaa69df2d913cf270a2568f53b95
Radoslav Hristov <160596999+RadoslavValentinov@users.noreply.github.com>
2024-05-25 09:25:45 +0300

    Add files via upload

---
+++ Streams Files and Directories/Line Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Line_Numbers_Exc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string inputText = "../../../text.txt";
+            string resultText = "../../../result.txt";
+            using (StreamWriter writeText = new StreamWriter(resultText))
+            {
+                using (StreamReader resdertext = new StreamReader(inputText))
+                {
+                    string lineOfText = resdertext.ReadLine();
+                    int count = 1;
+                    while (lineOfText !=null)
+                    {
+                        writeText.WriteLine($"Line {count}: {lineOfText}");
+                        count++;
+                        lineOfText = resdertext.ReadLine();
+                    }
+                }
+            }
+
+        }
+    }
+}
